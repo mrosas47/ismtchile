39b6d40ccce49d884b93b384c0dc80dee507fb08
mrosas47 <mrosas1690@gmail.com>
2022-09-15 13:50:27 -0300

    added functions data_export() and geoexport()

---
+++ R/data_export.R
@@ -0,0 +1,39 @@
+#' Export ISMT data
+#'
+#' @param df data.frame
+#' @param r integer
+#' @param path string
+#' @param format string
+#'
+#' @return message
+#' @export data_export
+#'
+#' @examples 'void for now'
+
+data_export <- function(df, r , path = '', format = 'rds') {
+
+  if (path == '') {
+
+    message('"path is void"; files will be saved to C:/ISMT_Files/data')
+
+    dir.create('C:/ISMT_Files')
+    dir.create('C:/ISMT_Files/data')
+    path = 'C:/ISMT_Files/data'
+
+  } else {
+
+    messages(glue('Files will be saved to {path}'))
+
+  }
+
+  if (format == 'rds') {
+
+    df %>% saveRDS(glue('{path}/ISMT_R{r}_data.rds'))
+
+  } else if (format == 'csv') {
+
+    df %>% write.csv2(glue('{path}/ISMT_R{r}_data.csv'))
+
+  }
+
+}
---
+++ R/geoexport.R
@@ -0,0 +1,35 @@
+#' Export geodata
+#'
+#' @param df data.frame. Expects processed shapefile data frame after geomerge()
+#' @param r integer. Working region
+#' @param path string. Path where files should be saved. Default is '', which creates C:/ISMT_Files/geodata
+#'
+#' @return message
+#' @export geoexport
+#'
+#' @import tidyverse
+#' @import sf
+#'
+#' @examples 'void for now'
+
+geoexport <- function(df, r, path = '') {
+
+  if (path == '') {
+
+    message('"path is void"; files will be saved to C:/ISMT_Files/geodata')
+
+    dir.create('C:/ISMT_Files')
+    dir.create('C:/ISMT_Files/geodata')
+    path = 'C:/ISMT_Files/geodata'
+
+  } else {
+
+    messages(glue('Files will be saved to {path}'))
+
+  }
+
+  df %>% write_sf(glue('{path}/ISMT_R{r}_geodata.shp'))
+
+  message('Geodata successfully exported!')
+
+}
